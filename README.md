f2a30860b6efae11c949db25d5ffd77adcc2e821
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-06-24 19:23:15 +1000

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+## ;TLDR
+
+An app to alert / display changes in forex spot prices
+
+## Demo
+
+## Instructions
+
+## Brief
+
+## High Level Design Thoughts
+
+## Assumptions on Input data
---
+++ src/run.test.ts
@@ -0,0 +1,24 @@
+import run, { DOWN, ENTER } from 'inquirer-test'
+
+const SPACEBAR = '\x20'
+
+// This spec requires a rerun of jest as it relies on node and the latest compiled run.js
+const runPath = `${process.cwd()}/build/run.js`
+const mockTournamentFilePath = `${process.cwd()}/testlib/fixtures/mock_tournament.txt`
+
+test('the app runs', async () => {
+	const result = await run([runPath, mockTournamentFilePath], [DOWN])
+
+	expect(result).toMatch(new RegExp('Query Match', 'g'))
+})
+
+test('getting the match result and games won and loss in tournament', async () => {
+	const result = await run(
+		[runPath, `${process.cwd()}/testlib/fixtures/mock_tournament.txt`],
+		[SPACEBAR, ENTER, DOWN, SPACEBAR, ENTER]
+	)
+
+	expect(result).toMatch(new RegExp('Person X defeated Person Y', 'g'))
+	expect(result).toMatch(new RegExp('2 sets to 1', 'g'))
+	expect(result).toMatch(new RegExp('Person X Games Stats: Won - 12, Lost - 6', 'g'))
+})
