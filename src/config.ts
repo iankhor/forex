33e07c3369d259b7ad161c73fb1f96669774c56b
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-06-25 23:01:46 +1000

    Add Rates class

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ import path from 'path'
 /**
  * Constants for app
  */
-
 export const STREAM_DATA_RATE_MILISECONDS = 200
+export const AVERAGE_PERIOD_SECONDS = 3 //5 minutes
 export const DEFAULT_DATA_FILE_PATH = path.join(process.cwd(), 'data', 'input1.jsonl')
